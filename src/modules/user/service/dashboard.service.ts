300270b92a1971119478cadac165d25f9d2bcb9b
RafaelSR0x <raffssantana007@gmail.com>
2026-03-31 15:53:30 -0300

    fix(dashboard-service): logic of returning the user referring to the access-token

--- src/modules/user/service/dashboard.service.ts
+++ src/modules/user/service/dashboard.service.ts
@@ -32,6 +32,42 @@ export class DashboardService {
   constructor(private readonly prisma: PrismaService) {}
 
   async getProfileByUserId(userId: string, role: string) {
-    
+    const userRole = role.toLocaleLowerCase();
+
+    //Advogado
+    if (userRole === 'lawyer') {
+      const lawyer = await this.prisma.lawyer.findUnique({
+        where: { id: userId },
+        select: DASHBOARD_FIELDS.lawyer,
+      });
+
+      if (!lawyer) {
+        throw new NotFoundException('Advogado não encontrado');
+      }
+
+      return {
+        role: 'Lawyer',
+        user: lawyer,
+      };
+    }
+
+    //Cidadão
+    if (userRole === 'citizen') {
+      const citizen = await this.prisma.citizen.findUnique({
+        where: { id: userId },
+        select: DASHBOARD_FIELDS.citizen,
+      });
+
+      if (!citizen) {
+        throw new NotFoundException('Cidadão não encontrado');
+      }
+
+      return {
+        role: 'Citizen',
+        user: citizen,
+      };
+    }
+
+    throw new BadRequestException('Role inválida');
   }
 }
