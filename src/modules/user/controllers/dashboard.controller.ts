3db81f53d9566563f572b31635ffdab633403c7d
RafaelSR0x <raffssantana007@gmail.com>
2026-03-31 15:57:39 -0300

    fix(dashboard-controller): using Decoration UseGuards to validate the JWT token and extract/inject the payload into request.user

--- src/modules/user/controllers/dashboard.controller.ts
+++ src/modules/user/controllers/dashboard.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Headers } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { ApiHeader, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { DashboardService } from '../service/dashboard.service';
+import { AuthTokenGuard } from 'src/modules/auth/guard/access-token.guard';
+import { Request } from 'express';
+
+interface AuthenticatedRequest extends Request {
+  user: {
+    sub: string;
+    role: string;
+  };
+}
 
 @ApiTags('dashboard')
 @Controller('dashboard')
@@ -8,6 +17,7 @@ export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
   @Get()
+  @UseGuards(AuthTokenGuard)
   @ApiHeader({
     name: 'access-token',
     required: true,
@@ -21,12 +31,9 @@ export class DashboardController {
     status: 401,
     description: 'Token ausente ou inválido',
   })
-  async getDashboard(
-    @Headers('access-token') accessToken: string,
-    @Headers('authorization') authorization: string,
-  ) {
-    return await this.dashboardService.getProfileByAccessToken(
-      accessToken || authorization,
-    );
+  async getDashboard(@Req() req: AuthenticatedRequest) {
+    const userId = req.user.sub;
+    const role = req.user.role;
+    return await this.dashboardService.getProfileByUserId(userId, role);
   }
 }
