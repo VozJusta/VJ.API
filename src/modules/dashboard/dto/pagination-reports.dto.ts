74ec6f308f50b52ce0acb9ab8502a946c1d05209
RafaelSR0x <raffssantana007@gmail.com>
2026-04-09 18:34:04 -0300

    fix:adjust name

--- src/modules/dashboard/dto/pagination-reports.dto.ts
+++ src/modules/dashboard/dto/pagination-reports.dto.ts
@@ -1,7 +1,7 @@
 import { IsInt, IsOptional, Min } from 'class-validator';
 import { Type } from 'class-transformer';
 
-export class PaginationReportsDto {
+export class PaginationReportsDTO {
   @IsOptional()
   @Type(() => Number)
   @IsInt()
