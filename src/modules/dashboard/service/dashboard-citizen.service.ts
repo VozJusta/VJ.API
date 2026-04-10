d5411ea2cc361e85e83fbd134ff7d06be13705b8
RafaelSR0x <raffssantana007@gmail.com>
2026-04-10 15:05:55 -0300

    feat(dashboard-service): separation of lawyer and citizen services

--- src/modules/dashboard/service/dashboard-citizen.service.ts
+++ src/modules/dashboard/service/dashboard-citizen.service.ts
@@ -8,7 +8,7 @@ import { PaginationReportsDTO } from '../dto/pagination-reports.dto';
 import { LawyerAnalyticsResponseDTO } from '../dto/lawyer-analytics.dto';
 
 @Injectable()
-export class DashboardService {
+export class DashboardCitizenService {
   constructor(private readonly prisma: PrismaService) {}
 
   async listReportsByCitizen(
@@ -132,48 +132,5 @@ export class DashboardService {
 
     throw new BadRequestException('Role inválida');
   }
-
-  async getAcceptedRequestAnalytics(
-    userId: string,
-    role: string,
-  ): Promise<LawyerAnalyticsResponseDTO> {
-    const userRole = role.toLowerCase();
-
-    if (userRole === 'lawyer') {
-      const lawyer = await this.prisma.lawyer.findFirst({
-        where: { id: userId },
-        select: { id: true },
-      });
-
-      if (!lawyer) {
-        throw new NotFoundException('Advogado não encontrado');
-      }
-      const acceptedReports = await this.prisma.report.findMany({
-        where: {
-          status: 'Accepted',
-          lawyer_id: userId,
-        },
-        select: { created_at: true },
-        orderBy: { created_at: 'asc' },
-      });
-
-      const groupedByDate = acceptedReports.reduce<Record<string, number>>(
-        (acc, report) => {
-          const date = report.created_at.toISOString().split('T')[0];
-          acc[date] = (acc[date] ?? 0) + 1;
-          return acc;
-        },
-        {},
-      );
-
-      return {
-        data: Object.entries(groupedByDate).map(([date, value]) => ({
-          date,
-          value,
-        })),
-      };
-    }
-
-    throw new BadRequestException('Role inválida');
-  }
+  
 }
