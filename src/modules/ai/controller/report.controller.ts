b7517679124be9df5774626dd4b8a20cc5b3212a
cau4alves <cau4alves@gmail.com>
2026-04-12 17:22:02 -0300

    feat(ai-reports): add start conversation route

--- src/modules/ai/controller/report.controller.ts
+++ src/modules/ai/controller/report.controller.ts
@@ -7,6 +7,7 @@ import { Request, Response } from "express";
 import { AuthTokenGuard } from "src/modules/auth/guard/access-token.guard";
 import { ApiBearerAuth, ApiBody, ApiHeader, ApiOperation, ApiParam, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { ReportResponseDto } from "../dto/report-response.dto";
+import { StartConversationDTO } from "../dto/start-conversation.dto";
 
 interface RequestUser extends Request {
     user: {
@@ -45,53 +46,53 @@ export class ReportController {
         type: ReportResponseDto 
     })
     @ApiResponse({ status: 401, description: 'Não autorizado (Token ausente ou inválido).' })
-    async create(@Body('text') text: string, @Req() req: RequestUser) {
-        return await this.reportService.createReport(text, req.user.sub)
+    async create(@Body() firstMessage: StartConversationDTO, @Req() req: RequestUser) {
+        return await this.reportService.startConversation(firstMessage, req.user.sub)
     }
 
-    @Put('accept/:id')
-    @ApiOperation({ summary: 'Aceita um relatório pendente vinculando-o ao advogado (Acesso restrito)' })
-    @ApiParam({ name: 'id', type: 'string', description: 'ID único do relatório gerado' })
-    @ApiResponse({ 
-        status: 200, 
-        description: 'Relatório aceito com sucesso e vinculado ao advogado.',
-        schema: { example: { message: 'Relatório aceito com sucesso' } }
-    })
-    @ApiResponse({ 
-        status: 401, 
-        description: 'Usuário não autorizado. Ocorre caso o token seja inválido ou o usuário seja um Cidadão (Citizen).',
-        schema: { example: { message: 'Usuário não autorizado', error: 'Unauthorized', statusCode: 401 } }
-    })
-    @ApiResponse({ 
-        status: 404, 
-        description: 'Relatório não encontrado no sistema.',
-        schema: { example: { message: 'Relatório não encontrado', error: 'Not Found', statusCode: 404 } }
-    })
-    async acceptReport(@Param('id') id: string, @Req() req: RequestUser) {
-        return await this.reportService.acceptReport(id, req.user.sub, req.user.role)
-    }
+    // @Put('accept/:id')
+    // @ApiOperation({ summary: 'Aceita um relatório pendente vinculando-o ao advogado (Acesso restrito)' })
+    // @ApiParam({ name: 'id', type: 'string', description: 'ID único do relatório gerado' })
+    // @ApiResponse({ 
+    //     status: 200, 
+    //     description: 'Relatório aceito com sucesso e vinculado ao advogado.',
+    //     schema: { example: { message: 'Relatório aceito com sucesso' } }
+    // })
+    // @ApiResponse({ 
+    //     status: 401, 
+    //     description: 'Usuário não autorizado. Ocorre caso o token seja inválido ou o usuário seja um Cidadão (Citizen).',
+    //     schema: { example: { message: 'Usuário não autorizado', error: 'Unauthorized', statusCode: 401 } }
+    // })
+    // @ApiResponse({ 
+    //     status: 404, 
+    //     description: 'Relatório não encontrado no sistema.',
+    //     schema: { example: { message: 'Relatório não encontrado', error: 'Not Found', statusCode: 404 } }
+    // })
+    // async acceptReport(@Param('id') id: string, @Req() req: RequestUser) {
+    //     return await this.reportService.acceptReport(id, req.user.sub, req.user.role)
+    // }
 
-    @Put('reject/:id')
-    @ApiOperation({ summary: 'Recusa um relatório pendente (Acesso restrito)' })
-    @ApiParam({ name: 'id', type: 'string', description: 'ID único do relatório gerado' })
-    @ApiResponse({ 
-        status: 200, 
-        description: 'Relatório recusado com sucesso.',
-        schema: { example: { message: 'Relatório recusado' } }
-    })
-    @ApiResponse({ 
-        status: 401, 
-        description: 'Usuário não autorizado. Ocorre caso o token seja inválido ou o usuário seja um Cidadão (Citizen).',
-        schema: { example: { message: 'Usuário não autorizado', error: 'Unauthorized', statusCode: 401 } }
-    })
-    @ApiResponse({ 
-        status: 404, 
-        description: 'Relatório não encontrado no sistema.',
-        schema: { example: { message: 'Relatório não encontrado', error: 'Not Found', statusCode: 404 } }
-    })
-    async rejectReport(@Param('id') id: string, @Req() req: RequestUser) {
-        return await this.reportService.rejectReport(id, req.user.role)
-    }
+    // @Put('reject/:id')
+    // @ApiOperation({ summary: 'Recusa um relatório pendente (Acesso restrito)' })
+    // @ApiParam({ name: 'id', type: 'string', description: 'ID único do relatório gerado' })
+    // @ApiResponse({ 
+    //     status: 200, 
+    //     description: 'Relatório recusado com sucesso.',
+    //     schema: { example: { message: 'Relatório recusado' } }
+    // })
+    // @ApiResponse({ 
+    //     status: 401, 
+    //     description: 'Usuário não autorizado. Ocorre caso o token seja inválido ou o usuário seja um Cidadão (Citizen).',
+    //     schema: { example: { message: 'Usuário não autorizado', error: 'Unauthorized', statusCode: 401 } }
+    // })
+    // @ApiResponse({ 
+    //     status: 404, 
+    //     description: 'Relatório não encontrado no sistema.',
+    //     schema: { example: { message: 'Relatório não encontrado', error: 'Not Found', statusCode: 404 } }
+    // })
+    // async rejectReport(@Param('id') id: string, @Req() req: RequestUser) {
+    //     return await this.reportService.rejectReport(id, req.user.role)
+    // }
 
     @Get('/pdf/:id')
     @ApiOperation({ summary: 'Gera e faz o download de um relatório em formato PDF' })
--- src/modules/ai/services/report.service.ts
+++ src/modules/ai/services/report.service.ts
@@ -4,6 +4,10 @@ import { LlmService } from "./llm.service";
 import { PrismaService } from "src/modules/prisma/service/prisma.service";
 import { Specialization } from "generated/prisma/enums";
 import { NotFoundError } from "rxjs";
+import { finished } from "stream";
+import { StartConversationDTO } from "../dto/start-conversation.dto";
+
+const MAX_TURNS = 6;
 
 @Injectable()
 export class ReportService {
@@ -13,29 +17,155 @@ export class ReportService {
         private prisma: PrismaService
     ) { }
 
-    async createReport(text: string, userId: string) {
-        const classification = await this.llmService.generate({
-            input: `Classifique a área jurídica do texto: ${text}`,
-            context: []
-        });
+    // async createReport(text: string, userId: string) {
+    //     const classification = await this.llmService.generate({
+    //         input: `Classifique a área jurídica do texto: ${text}`,
+    //         context: []
+    //     });
 
-        const area = parseSpecialization(classification.output?.area)
+    //     const area = parseSpecialization(classification.output?.area)
 
-        const report = await this.prisma.report.create({
+    //     const report = await this.prisma.report.create({
+    //         data: {
+    //             transcription: text,
+    //             normalized_text: text,
+    //             legal_analysis: '',
+    //             simplified_explanation: '',
+    //             category_detected: area,
+    //             user_id: userId,
+    //             status: 'Pending'
+    //         }
+    //     })
+
+    //     let context = await this.ragService.retrieve(text, area)
+
+    //     context = context.slice(0, 3)
+
+    //     if (context.length > 0) {
+    //         await this.prisma.ragContext.createMany({
+    //             data: context.map((c) => ({
+    //                 report_id: report.id,
+    //                 source: (c.source as string) || 'qdrant',
+    //                 content: String(c.content),
+    //                 score: Number(c.score)
+    //             }))
+    //         })
+    //     }
+
+    //     const response = await this.llmService.generate({
+    //         input: text,
+    //         context,
+    //     })
+
+    //     await this.prisma.aiResponse.create({
+    //         data: {
+    //             report_id: report.id,
+    //             model: 'llama-3.1-8b-instant',
+    //             provider: 'groq',
+    //             prompt: response.prompt,
+    //             response: JSON.stringify(response.output)
+    //         }
+    //     })
+
+    //     await this.prisma.report.update({
+    //         where: { id: report.id },
+    //         data: {
+    //             legal_analysis: response.output.legal_analysis,
+    //             simplified_explanation: response.output.simplified_explanation,
+    //             category_detected: parseSpecialization(response.output.area),
+    //             status: 'Pending'
+    //         }
+    //     })
+
+    //     return {
+    //         input: text,
+    //         ...response.output,
+    //     }
+    // }
+
+    async startConversation(firstMessage: StartConversationDTO, userId: string) {
+        const newCase = await this.prisma.case.create({
             data: {
-                transcription: text,
-                normalized_text: text,
-                legal_analysis: '',
-                simplified_explanation: '',
-                category_detected: area,
+                title: firstMessage.message.slice(0, 60),
                 user_id: userId,
                 status: 'Pending'
             }
         })
 
-        let context = await this.ragService.retrieve(text, area)
+        const conversation = await this.prisma.conversation.create({
+            data: {
+                case_id: newCase.id,
+                messages: {
+                    create: {
+                        role: 'User',
+                        content: firstMessage.message,
+                    }
+                }
+            },
+            include: {
+                messages: true
+            }
+        })
 
-        context = context.slice(0, 3)
+        const { shouldGenerate, questionOrAck } = await this.llmService.chat([
+            { role: 'User', content: firstMessage.message }
+        ])
+
+        await this.prisma.message.create({
+            data: {
+                conversation_id: conversation.id,
+                role: 'Assistant',
+                content: questionOrAck
+            }
+        })
+
+        if (shouldGenerate) {
+            await this.generateReportFromConversation(conversation.id, newCase.id, userId)
+        }
+
+        return {
+            conversationId: conversation.id,
+            caseId: newCase.id,
+            question: questionOrAck,
+            finished: false,
+        }
+    }
+
+    private async generateReportFromConversation(
+        conversationId: string,
+        caseId: string,
+        userId: string
+    ) {
+        const conversation = await this.prisma.conversation.findUnique({
+            where: { id: conversationId },
+            include: { messages: { orderBy: { created_at: 'asc' } } }
+        });
+
+        const fullText = conversation?.messages
+            .map(m => `${m.role === 'User' ? 'Usuário' : 'Assistente'}: ${m.content}`)
+            .join('\n');
+
+        const classification = await this.llmService.generate({
+            input: `Classifique a área jurídica do texto: ${fullText}`,
+            context: []
+        });
+
+        const area = parseSpecialization(classification.output?.area);
+
+        const report = await this.prisma.report.create({
+            data: {
+                transcription: fullText || '',
+                normalized_text: fullText || '',
+                legal_analysis: '',
+                simplified_explanation: '',
+                category_detected: area,
+                user_id: userId,
+                caseId: caseId,
+            }
+        });
+
+        let context = await this.ragService.retrieve(fullText || '', area);
+        context = context.slice(0, 3);
 
         if (context.length > 0) {
             await this.prisma.ragContext.createMany({
@@ -45,13 +175,13 @@ export class ReportService {
                     content: String(c.content),
                     score: Number(c.score)
                 }))
-            })
+            });
         }
 
         const response = await this.llmService.generate({
-            input: text,
+            input: fullText || '',
             context,
-        })
+        });
 
         await this.prisma.aiResponse.create({
             data: {
@@ -61,7 +191,7 @@ export class ReportService {
                 prompt: response.prompt,
                 response: JSON.stringify(response.output)
             }
-        })
+        });
 
         await this.prisma.report.update({
             where: { id: report.id },
@@ -69,70 +199,77 @@ export class ReportService {
                 legal_analysis: response.output.legal_analysis,
                 simplified_explanation: response.output.simplified_explanation,
                 category_detected: parseSpecialization(response.output.area),
-                status: 'Pending'
             }
-        })
+        });
+
+        await this.prisma.conversation.update({
+            where: { id: conversationId },
+            data: { is_closed: true }
+        });
 
         return {
-            input: text,
+            finished: true,
+            caseId,
+            reportId: report.id,
+            input: fullText,
             ...response.output,
-        }
+        };
     }
 
-    async acceptReport(reportId: string, lawyerId: string, role: string) {
-        const report = await this.prisma.report.findUnique({
-            where: {
-                id: reportId
-            }
-        })
+    // async acceptReport(reportId: string, lawyerId: string, role: string) {
+    //     const report = await this.prisma.report.findUnique({
+    //         where: {
+    //             id: reportId
+    //         }
+    //     })
 
-        if (!report) {
-            throw new NotFoundException('Relatório não encontrado')
-        }
+    //     if (!report) {
+    //         throw new NotFoundException('Relatório não encontrado')
+    //     }
 
-        if (role === "Citizen") {
-            throw new UnauthorizedException('Usuário não autorizado')
-        }
+    //     if (role === "Citizen") {
+    //         throw new UnauthorizedException('Usuário não autorizado')
+    //     }
 
-        await this.prisma.report.update({
-            where: { id: reportId },
-            data: {
-                status: 'Accepted',
-                lawyer_id: lawyerId
-            }
-        })
+    //     await this.prisma.report.update({
+    //         where: { id: reportId },
+    //         data: {
+    //             status: 'Accepted',
+    //             lawyer_id: lawyerId
+    //         }
+    //     })
 
-        return {
-            message: 'Relatório aceito com sucesso'
-        }
-    }
+    //     return {
+    //         message: 'Relatório aceito com sucesso'
+    //     }
+    // }
 
-    async rejectReport(reportId: string, role: string) {
-        const report = await this.prisma.report.findUnique({
-            where: {
-                id: reportId
-            }
-        })
+    // async rejectReport(reportId: string, role: string) {
+    //     const report = await this.prisma.report.findUnique({
+    //         where: {
+    //             id: reportId
+    //         }
+    //     })
 
-        if (!report) {
-            throw new NotFoundException('Relatório não encontrado')
-        }
-        
-        if (role === "Citizen") {
-            throw new UnauthorizedException('Usuário não autorizado')
-        }
+    //     if (!report) {
+    //         throw new NotFoundException('Relatório não encontrado')
+    //     }
 
-        await this.prisma.report.update({
-            where: { id: reportId },
-            data: {
-                status: 'Refused',
-            }
-        })
+    //     if (role === "Citizen") {
+    //         throw new UnauthorizedException('Usuário não autorizado')
+    //     }
 
-        return {
-            message: 'Relatório recusado'
-        }
-    }
+    //     await this.prisma.report.update({
+    //         where: { id: reportId },
+    //         data: {
+    //             status: 'Refused',
+    //         }
+    //     })
+
+    //     return {
+    //         message: 'Relatório recusado'
+    //     }
+    // }
 }
 
 function parseSpecialization(area: string): Specialization {
