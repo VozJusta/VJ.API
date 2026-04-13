d39dbb684175364fd2d75f2278422a9d9ebe7faa
RafaelSR0x <raffssantana007@gmail.com>
2026-04-13 16:42:50 -0300

    update src/modules/user/citizen.module.ts

--- src/modules/user/citizen.module.ts
+++ src/modules/user/citizen.module.ts
@@ -2,12 +2,6 @@ import { Module } from '@nestjs/common';
 import { CitizenService } from './service/citizen.service';
 import { CitizenController } from './controllers/citizen.controller';
 import { PrismaModule } from 'src/modules/prisma/prisma.module';
-import { SecurityTokenInterceptor } from '../auth/interceptors/security-token.interceptor';
-import { ConfigModule, ConfigType } from '@nestjs/config';
-import jwtConfig from '../auth/config/jwt.config';
-import { JwtModule } from '@nestjs/jwt';
-import { DashboardCitizenController } from '../dashboard/controllers/dashboard-citizen.controller';
-import { DashboardCitizenService } from '../dashboard/service/dashboard-citizen.service';
 import { AuthTokenGuard } from '../auth/guard/access-token.guard';
 import { AuthModule } from '../auth/auth.module';
 
@@ -16,7 +10,7 @@ import { AuthModule } from '../auth/auth.module';
     AuthModule,
     PrismaModule,
   ],
-  providers: [CitizenService, DashboardCitizenService, AuthTokenGuard],
-  controllers: [CitizenController, DashboardCitizenController]
+  providers: [CitizenService, AuthTokenGuard],
+  controllers: [CitizenController]
 })
 export class CitizenModule { }
