81df4f77ef7e7cd79db9d76a68a09f35d7546876
RafaelSR0x <raffssantana007@gmail.com>
2026-04-22 02:54:07 -0300

    feat(lawyer-module): Add new services and controllers to the module.

--- src/modules/lawyer/module/lawyer.module.ts
+++ src/modules/lawyer/module/lawyer.module.ts
@@ -4,23 +4,27 @@ import { LawyerController } from '@m/lawyer/controllers/lawyer.controller';
 import { PrismaModule } from '@m/prisma/prisma.module';
 import { SecurityTokenInterceptor } from '@m/auth/interceptors/security-token.interceptor';
 import { AuthModule } from '@m/auth/module/auth.module';
-import { LawyerRequestsService } from '@m/lawyer/service/lawyer-requests.service';
+import { LawyerRequestsStatusService } from '@modules/lawyer/service/lawyerRequestsStatus.service';
 import { LawyerRequestController } from '@m/lawyer/controllers/lawyer-requests.controller';
-import { CaseRequestService } from '@m/lawyer/service/case-request.service';
-import { CaseRequestController } from '@m/lawyer/controllers/case-request.controller';
+import { AcceptCaseRequest } from '@m/lawyer/service/acceptCaseRequest.service';
+import { RejectCaseRequest } from '@m/lawyer/service/rejectCaseRequest.service';
+import { AcceptCaseRequestController } from '@m/lawyer/controllers/patchAcceptCaseRequest.controller';
+import { RejectCaseRequestController } from '@m/lawyer/controllers/patchRejectCaseRequest.controller';
 
 @Module({
   imports: [AuthModule, PrismaModule],
   providers: [
     LawyerService,
-    LawyerRequestsService,
-    CaseRequestService,
+    LawyerRequestsStatusService,
+    AcceptCaseRequest,
+    RejectCaseRequest,
     SecurityTokenInterceptor,
   ],
   controllers: [
     LawyerController,
     LawyerRequestController,
-    CaseRequestController,
+    AcceptCaseRequestController,
+    RejectCaseRequestController,
   ],
 })
 export class LawyerModule {}
