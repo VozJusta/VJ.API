9a1d11f467c2289dfc7ed19091089d13d1d26ac0
RafaelSR0x <raffssantana007@gmail.com>
2026-04-22 02:59:14 -0300

    fix(lawyer-module): update import

--- src/modules/lawyer/module/lawyer.module.ts
+++ src/modules/lawyer/module/lawyer.module.ts
@@ -5,7 +5,7 @@ import { PrismaModule } from '@m/prisma/prisma.module';
 import { SecurityTokenInterceptor } from '@m/auth/interceptors/security-token.interceptor';
 import { AuthModule } from '@m/auth/module/auth.module';
 import { LawyerRequestsStatusService } from '@modules/lawyer/service/lawyerRequestsStatus.service';
-import { LawyerRequestController } from '@m/lawyer/controllers/lawyer-requests.controller';
+import { LawyerRequestController } from '@modules/lawyer/controllers/lawyerRequests.controller';
 import { AcceptCaseRequest } from '@m/lawyer/service/acceptCaseRequest.service';
 import { RejectCaseRequest } from '@m/lawyer/service/rejectCaseRequest.service';
 import { AcceptCaseRequestController } from '@m/lawyer/controllers/patchAcceptCaseRequest.controller';
