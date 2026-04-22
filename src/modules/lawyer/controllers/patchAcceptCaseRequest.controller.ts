2f7adaecec4f006a75ad91e484261b2ad0d40a06
RafaelSR0x <raffssantana007@gmail.com>
2026-04-22 02:57:24 -0300

    feat(lawyer-controllers): route to accept the CaseRequest

---
+++ src/modules/lawyer/controllers/patchAcceptCaseRequest.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Param,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiHeader,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { AuthTokenGuard } from 'src/modules/auth/guard/access-token.guard';
+import { Request } from 'express';
+import { AcceptCaseRequest } from '../service/acceptCaseRequest.service';
+
+interface AuthenticatedRequest extends Request {
+  user: {
+    sub: string;
+    role: string;
+  };
+}
+
+@Controller('lawyer')
+@ApiTags('Case Requests')
+@ApiBearerAuth()
+export class AcceptCaseRequestController {
+  constructor(private readonly acceptCaseRequestService: AcceptCaseRequest) {}
+
+  @Put('requests/:id/accept')
+  @UseGuards(AuthTokenGuard)
+  async acceptCaseRequest(
+    @Param('id') caseRequestId: string,
+    @Req() req: AuthenticatedRequest,
+  ) {
+    const userId = req.user.sub;
+    const role = req.user.role;
+
+    return this.acceptCaseRequestService.acceptCaseRequest(
+      userId,
+      role,
+      caseRequestId,
+    );
+  }
+}
