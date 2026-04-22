ce7988a3ec3419bda90510920f9732ac30ea9f7b
RafaelSR0x <raffssantana007@gmail.com>
2026-04-22 02:58:05 -0300

    feat(lawyer-controllers): route to reject the CaseRequest

---
+++ src/modules/lawyer/controllers/patchRejectCaseRequest.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Param,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiHeader,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { AuthTokenGuard } from 'src/modules/auth/guard/access-token.guard';
+import { Request } from 'express';
+import { RejectCaseRequest } from '../service/rejectCaseRequest.service';
+
+interface AuthenticatedRequest extends Request {
+  user: {
+    sub: string;
+    role: string;
+  };
+}
+
+@Controller('lawyer')
+@ApiTags('Case Requests')
+@ApiBearerAuth()
+export class RejectCaseRequestController {
+  constructor(private readonly rejectCaseRequestService: RejectCaseRequest) {}
+
+  @Put('requests/:id/reject')
+  @UseGuards(AuthTokenGuard)
+  async rejectCaseRequest(
+    @Param('id') caseRequestId: string,
+    @Req() req: AuthenticatedRequest,
+  ) {
+    const userId = req.user.sub;
+    const role = req.user.role;
+
+    return this.rejectCaseRequestService.rejectCaseRequest(
+      userId,
+      role,
+      caseRequestId,
+    );
+  }
+}
