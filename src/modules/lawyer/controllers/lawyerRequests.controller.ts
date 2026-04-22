025cdb4421e3a9010eec51575965339f44a8d827
RafaelSR0x <raffssantana007@gmail.com>
2026-04-22 02:56:37 -0300

    feat(lawyer-controllers): route to fetch CaseRequest by status

---
+++ src/modules/lawyer/controllers/lawyerRequests.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
+import { AuthTokenGuard } from 'src/modules/auth/guard/access-token.guard';
+import { LawyerRequestsStatusService } from '../service/lawyerRequestsStatus.service';
+import { Request } from 'express';
+import { RequestsStatusDTO } from '../dto/requests-status.dto';
+
+interface AuthenticateRequest extends Request {
+  user: {
+    sub: string;
+    role: string;
+  };
+}
+
+@Controller('lawyer')
+export class LawyerRequestController {
+  constructor(private readonly lawyerRequestStatus: LawyerRequestsStatusService) {}
+
+  @Get('/requests')
+  @UseGuards(AuthTokenGuard)
+  async getReportsByStatus(
+    @Req() req: AuthenticateRequest,
+    @Query() status?: RequestsStatusDTO,
+  ) {
+    const userId = req.user.sub;
+    const role = req.user.role;
+
+    return this.lawyerRequestStatus.requestsByStatus(userId, role, status);
+  }
+}
