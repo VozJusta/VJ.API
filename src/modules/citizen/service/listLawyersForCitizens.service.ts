b1fdfcc73a198b54a6aad82c30a96eecec001f2d
RafaelSR0x <raffssantana007@gmail.com>
2026-04-23 19:36:27 -0300

    feat(citizen-service): service to list lawyers

--- src/modules/citizen/service/listLawyersForCitizens.service.ts
+++ src/modules/citizen/service/listLawyersForCitizens.service.ts
@@ -5,5 +5,20 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 export class ListLawyersForCitizens {
   constructor(private readonly prisma: PrismaService) {}
 
-  
+  async listLawyers(userId: string, role: string, lawyerId: string) {
+    const userRole = role.toLowerCase();
+
+    if (userRole === 'citizen') {
+      const citizen = await this.prisma.citizen.findFirst({
+        where: { id: userId },
+        select: { id: true },
+      });
+
+      if(!citizen) {
+        throw new NotFoundException('Cidadão não encontrado');
+      }
+
+
+    }
+  }
 }
