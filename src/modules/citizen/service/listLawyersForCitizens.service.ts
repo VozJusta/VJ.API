6b8d6e2f7af74cb6b278aed5769d32c152b71050
RafaelSR0x <raffssantana007@gmail.com>
2026-04-23 20:12:31 -0300

    feat(citizen-service): A service that lists all lawyers with Verified status from the database.

--- src/modules/citizen/service/listLawyersForCitizens.service.ts
+++ src/modules/citizen/service/listLawyersForCitizens.service.ts
@@ -1,11 +1,15 @@
 import { PrismaService } from '@modules/prisma/service/prisma.service';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 
 @Injectable()
 export class ListLawyersForCitizens {
   constructor(private readonly prisma: PrismaService) {}
 
-  async listLawyers(userId: string, role: string, lawyerId: string) {
+  async listLawyers(userId: string, role: string) {
     const userRole = role.toLowerCase();
 
     if (userRole === 'citizen') {
@@ -14,11 +18,30 @@ export class ListLawyersForCitizens {
         select: { id: true },
       });
 
-      if(!citizen) {
+      if (!citizen) {
         throw new NotFoundException('Cidadão não encontrado');
       }
 
+      const findAllLawers = await this.prisma.lawyer.findMany({
+        where: { lawyer_status: 'Verified' },
+        select: {
+          id: true,
+          full_name: true,
+          specialization: true,
+          avatar_image: true,
+          rating: true,
+        },
+      });
 
+      return findAllLawers.map((lawyer) => ({
+        id: lawyer.id,
+        fullName: lawyer.full_name,
+        specialization: lawyer.specialization ?? '',
+        avatar_url: lawyer.avatar_image ?? '',
+        rating: lawyer.rating ? Number(lawyer.rating) : 0,
+      }));
     }
+
+    throw new BadRequestException('Role inválida');
   }
 }
