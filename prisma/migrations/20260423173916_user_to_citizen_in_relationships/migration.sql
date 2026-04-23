42d618730ec4d3262d7082338a825f77c77b16e1
RafaelSR0x <raffssantana007@gmail.com>
2026-04-23 14:42:43 -0300

    refactor(prisma): Update the user and userId fields to citizen and citizenId.

---
+++ prisma/migrations/20260423173916_user_to_citizen_in_relationships/migration.sql
@@ -0,0 +1,96 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `user_id` on the `Case` table. All the data in the column will be lost.
+  - You are about to drop the column `user_id` on the `Report` table. All the data in the column will be lost.
+  - You are about to drop the column `reportId` on the `Simulation` table. All the data in the column will be lost.
+  - You are about to drop the column `user_id` on the `Simulation` table. All the data in the column will be lost.
+  - You are about to drop the column `user_id` on the `SimulationReport` table. All the data in the column will be lost.
+  - You are about to drop the column `user_id` on the `Subscription` table. All the data in the column will be lost.
+  - You are about to drop the column `user_id` on the `ValidationCode` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[citizen_id]` on the table `SimulationReport` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[citizen_id]` on the table `Subscription` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `citizen_id` to the `Case` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `citizen_id` to the `Report` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `citizen_id` to the `Simulation` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `citizen_id` to the `SimulationReport` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Case" DROP CONSTRAINT "Case_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Report" DROP CONSTRAINT "Report_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Simulation" DROP CONSTRAINT "Simulation_reportId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Simulation" DROP CONSTRAINT "Simulation_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "SimulationReport" DROP CONSTRAINT "SimulationReport_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Subscription" DROP CONSTRAINT "Subscription_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ValidationCode" DROP CONSTRAINT "ValidationCode_user_id_fkey";
+
+-- DropIndex
+DROP INDEX "SimulationReport_user_id_key";
+
+-- DropIndex
+DROP INDEX "Subscription_user_id_key";
+
+-- AlterTable
+ALTER TABLE "Case" DROP COLUMN "user_id",
+ADD COLUMN     "citizen_id" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Report" DROP COLUMN "user_id",
+ADD COLUMN     "citizen_id" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Simulation" DROP COLUMN "reportId",
+DROP COLUMN "user_id",
+ADD COLUMN     "citizen_id" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "SimulationReport" DROP COLUMN "user_id",
+ADD COLUMN     "citizen_id" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Subscription" DROP COLUMN "user_id",
+ADD COLUMN     "citizen_id" TEXT;
+
+-- AlterTable
+ALTER TABLE "ValidationCode" DROP COLUMN "user_id",
+ADD COLUMN     "citizen_id" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SimulationReport_citizen_id_key" ON "SimulationReport"("citizen_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Subscription_citizen_id_key" ON "Subscription"("citizen_id");
+
+-- AddForeignKey
+ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_citizen_id_fkey" FOREIGN KEY ("citizen_id") REFERENCES "Citizen"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Report" ADD CONSTRAINT "Report_citizen_id_fkey" FOREIGN KEY ("citizen_id") REFERENCES "Citizen"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Case" ADD CONSTRAINT "Case_citizen_id_fkey" FOREIGN KEY ("citizen_id") REFERENCES "Citizen"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ValidationCode" ADD CONSTRAINT "ValidationCode_citizen_id_fkey" FOREIGN KEY ("citizen_id") REFERENCES "Citizen"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Simulation" ADD CONSTRAINT "Simulation_citizen_id_fkey" FOREIGN KEY ("citizen_id") REFERENCES "Citizen"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Simulation" ADD CONSTRAINT "Simulation_report_id_fkey" FOREIGN KEY ("report_id") REFERENCES "Report"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SimulationReport" ADD CONSTRAINT "SimulationReport_citizen_id_fkey" FOREIGN KEY ("citizen_id") REFERENCES "Citizen"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
