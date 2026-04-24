f17245b5151e21a460f4b3564e2cdb9a4cfbdad2
RafaelSR0x <raffssantana007@gmail.com>
2026-04-23 23:04:58 -0300

    feat(citizen-dto): create pagination lawyers

---
+++ src/modules/citizen/dto/pagination-lawyers.dto.ts
@@ -0,0 +1,16 @@
+import { IsInt, IsOptional, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class PaginationLawyersDTO {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  pageSize?: number = 2;
+}
