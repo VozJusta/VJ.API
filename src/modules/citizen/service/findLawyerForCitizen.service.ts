e3c4b74352eb7905d0dfa8abe966b914ab24bfa2
RafaelSR0x <raffssantana007@gmail.com>
2026-04-23 21:05:23 -0300

    feat(citizen-service): service to display lawyer information to the citizen

--- src/modules/citizen/service/findLawyerForCitizen.service.ts
+++ src/modules/citizen/service/findLawyerForCitizen.service.ts
@@ -1,6 +1,43 @@
-import { Injectable } from "@nestjs/common";
+import { PrismaService } from '@modules/prisma/service/prisma.service';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class FindLawyerForCitizen {
-    
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findLawyerForCitizen(userId: string, role: string, lawyerId: string) {
+    const userRole = role.toLowerCase();
+
+    if (userRole === 'citizen') {
+      const citizen = await this.prisma.citizen.findFirst({
+        where: { id: userId },
+        select: { id: true },
+      });
+
+      if (!citizen) {
+        throw new NotFoundException('Cidadão não encontrado');
+      }
+
+      const allInfoLawyer = await this.prisma.lawyer.findFirst({
+        where: { id: lawyerId },
+        select: {
+          id: true,
+          full_name: true,
+          specialization: true,
+          avatar_image: true,
+          oab_number: true,
+          oab_state: true,
+          bio: true,
+        },
+      });
+
+      if(!allInfoLawyer) {
+        throw new NotFoundException('Advogado não encontrado');
+      }
+
+      return allInfoLawyer
+    }
+
+    throw new BadRequestException('Role inválida');
+  }
 }
