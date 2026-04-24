9eea4141a0c981f584facea4d1ddb017b932e7a0
RafaelSR0x <raffssantana007@gmail.com>
2026-04-23 21:07:47 -0300

    update citizen.module

--- src/modules/citizen/module/citizen.module.ts
+++ src/modules/citizen/module/citizen.module.ts
@@ -5,10 +5,12 @@ import { PrismaModule } from '@m/prisma/prisma.module';
 import { AuthModule } from '@m/auth/module/auth.module'; 
 import { GetLawyersForCitizen } from '@m/citizen/controllers/getLawyersForCitizen.controller';
 import { ListLawyersForCitizens } from '@m/citizen/service/listLawyersForCitizens.service';
+import { FindLawyerForCitizen } from '../service/findLawyerForCitizen.service';
+import { GetOneLawyerForCitizen } from '../controllers/getOneLawyerForCitizen.controller';
 
 @Module({
   imports: [AuthModule, PrismaModule],
-  providers: [CitizenService, ListLawyersForCitizens],
-  controllers: [CitizenController, GetLawyersForCitizen],
+  providers: [CitizenService, ListLawyersForCitizens, FindLawyerForCitizen],
+  controllers: [CitizenController, GetLawyersForCitizen, GetOneLawyerForCitizen],
 })
 export class CitizenModule {}
