d05ddf9fb82329017c4b06bca26da61d683fce76
RafaelSR0x <raffssantana007@gmail.com>
2026-04-28 13:31:08 -0300

    feat(notifications-module): create notification module

---
+++ src/modules/notifications/module/notifications.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { LawyerService } from '@m/lawyer/service/lawyer.service';
+import { LawyerController } from '@m/lawyer/controllers/lawyer.controller';
+import { PrismaModule } from '@m/prisma/prisma.module';
+import { SecurityTokenInterceptor } from '@m/auth/interceptors/security-token.interceptor';
+import { AuthModule } from '@m/auth/module/auth.module';
+import { LawyerRequestsStatusService } from '@m/lawyer/service/lawyerRequestsStatus.service';
+import { LawyerRequestController } from '@m/lawyer/controllers/lawyerRequests.controller';
+import { AcceptCaseRequest } from '@m/lawyer/service/acceptCaseRequest.service';
+import { RejectCaseRequest } from '@m/lawyer/service/rejectCaseRequest.service';
+import { AcceptCaseRequestController } from '@m/lawyer/controllers/patchAcceptCaseRequest.controller';
+import { RejectCaseRequestController } from '@m/lawyer/controllers/patchRejectCaseRequest.controller';
+import { FindCaseById } from '@m/lawyer/service/findCaseById.service';
+import { GetCaseByIdController } from '@m/lawyer/controllers/getCaseById.controller';
+
+@Module({
+  imports: [AuthModule, PrismaModule],
+  providers: [],
+  controllers: [],
+  exports: [],
+})
+export class LawyerModule {}
