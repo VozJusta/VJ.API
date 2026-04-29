46da5f065ce610955c9983ed784f157a931fdb19
RafaelSR0x <raffssantana007@gmail.com>
2026-04-29 16:56:42 -0300

    feat(notifications-controllers): controller to list notifications

---
+++ src/modules/notifications/controllers/getAllNotifications.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
+import { ReadAllNotifications } from '../service/readAllNotifications.service';
+import { AuthTokenGuard } from '@modules/auth/guard/access-token.guard';
+import { RequestUser } from '@modules/common/interfaces/interfaces';
+import { PaginationNotificationsDTO } from '@m/notifications/dto/pagination-notifications.dto';
+
+@Controller('notifications')
+export class GetAllNotifications {
+  constructor(private readonly readAllNotifications: ReadAllNotifications) {}
+
+  @Get()
+  @UseGuards(AuthTokenGuard)
+  async getAllNotifications(@Req() req: RequestUser, @Query() pagination: PaginationNotificationsDTO){
+    const userId = req.user.sub
+    const role = req.user.role
+
+    return this.readAllNotifications.readAll(userId, role, pagination)
+  }
+}
