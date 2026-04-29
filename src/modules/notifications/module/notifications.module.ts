85734cc0b0ef9ca465c2bdab795331ed9215b5b9
RafaelSR0x <raffssantana007@gmail.com>
2026-04-29 16:58:56 -0300

    feat(notifications-module): I added service and controller to module

--- src/modules/notifications/module/notifications.module.ts
+++ src/modules/notifications/module/notifications.module.ts
@@ -3,11 +3,13 @@ import { AuthModule } from '@m/auth/module/auth.module';
 import { PrismaModule } from '@m/prisma/prisma.module';
 import { NotificationsGateway } from '@m/notifications/gateway/notifications.gateway';
 import { NotificationsService } from '../service/notifications.service';
+import { ReadAllNotifications } from '../service/readAllNotifications.service';
+import { GetAllNotifications } from '../controllers/getAllNotifications.controller';
 
 @Module({
   imports: [AuthModule, PrismaModule],
-  providers: [NotificationsGateway, NotificationsService],
-  controllers: [],
+  providers: [NotificationsGateway, NotificationsService, ReadAllNotifications],
+  controllers: [GetAllNotifications],
   exports: [NotificationsGateway, NotificationsService],
 })
 export class NotificationsModule {}
