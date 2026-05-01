21a509d8b66c97323425728de7e378a76829fce3
RafaelSR0x <raffssantana007@gmail.com>
2026-05-01 13:11:21 -0300

    feat(notifications-controllers): the controller for the deleteOneNotification.service service is used to delete a notification

---
+++ src/modules/notifications/controllers/deleteOneNotifications.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Delete, Param, Req, UseGuards } from '@nestjs/common';
+import { AuthTokenGuard } from '@modules/auth/guard/access-token.guard';
+import { RequestUser } from '@modules/common/interfaces/interfaces';
+import { DeleteNotificationsByIdService } from '@m/notifications/service/deleteOneNotifications.service';
+
+@Controller('notifications')
+export class DeleteNotificationsByIdController {
+  constructor(private readonly deleteNotifications: DeleteNotificationsByIdService) {}
+
+  @Delete(':notificationId')
+  @UseGuards(AuthTokenGuard)
+  async deleteNotification(
+    @Req() req: RequestUser,
+    @Param('notificationId') notificationId: string,
+  ) {
+    const userId = req.user.sub;
+    const role = req.user.role;
+
+    return this.deleteNotifications.deleteOne(userId, role, notificationId);
+  }
+}
