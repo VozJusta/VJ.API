1095621637b5dc2275aa74029c65c8ddc59bc166
ooliveiratg <menezesth16@gmail.com>
2026-05-01 12:36:55 -0300

    feat(payments): if(citizen){

--- src/modules/payments/service/costumer/create.service.ts
+++ src/modules/payments/service/costumer/create.service.ts
@@ -17,35 +17,58 @@ export class CreateCustomerService {
   ) {}
 
   async CreateCustomer(name: string, email: string) {
-    if (!email) {
-      throw new UnauthorizedException('Email obrigatório');
-    }
-    const citizen = await this.prisma.citizen.findUnique({
-      where: { email: email },
-    });
-    const lawyer = await this.prisma.lawyer.findUnique({
-      where: { email: email },
-    });
+    try {
+      if (!email) {
+        throw new UnauthorizedException('Email obrigatório');
+      }
 
-    if (!citizen && !lawyer) {
-      throw new UnauthorizedException('Usuário não encontrado');
+      const existingCustomer = await this.stripeClient.customers.list({
+        email: email,
+        limit: 1,
+      });
+      if (existingCustomer.data.length > 0) {
+        throw new BadRequestException('Cliente já existe');
+      }
+
+      const citizen = await this.prisma.citizen.findUnique({
+        where: { email: email },
+      });
+      const lawyer = await this.prisma.lawyer.findUnique({
+        where: { email: email },
+      });
+
+      if (!citizen && !lawyer) {
+        throw new UnauthorizedException('Usuário não encontrado');
+      }
+
+      const customer = await this.stripeClient.customers.create({
+        name: name,
+        email: email,
+      });
+
+      if (citizen) {
+        await this.prisma.citizen.update({
+          where: { email: email },
+          data: {
+            stripe_customer_id: customer.id,
+          },
+        });
+      } else if (lawyer) {
+        await this.prisma.lawyer.update({
+          where: { email: email },
+          data: {
+            stripe_customer_id: customer.id,
+          },
+        });
+      }
+      return {
+        id: customer.id,
+        name: customer.name,
+        email: customer.email,
+        createdAt: customer.created,
+      };
+    } catch (error) {
+      throw new BadRequestException('Erro ao criar cliente: ' + error.message);
     }
-    const existingCustomer = await this.stripeClient.customers.list({
-      email: email,
-      limit: 1,
-    });
-    if (existingCustomer.data.length > 0) {
-      throw new BadRequestException('Cliente já existe');
-    }
-    const customer = await this.stripeClient.customers.create({
-      name: name,
-      email: email,
-    });
-    return {
-      id: customer.id,
-      name: customer.name,
-      email: customer.email,
-      createdAt: customer.created,
-    };
   }
 }
