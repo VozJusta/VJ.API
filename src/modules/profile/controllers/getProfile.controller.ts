7c6165977cf412b4430f95de606bfe5f8ab693e9
RafaelSR0x <raffssantana007@gmail.com>
2026-05-04 16:54:54 -0300

    feat(profile-controllers): controller to display user profiles handling roles.

---
+++ src/modules/profile/controllers/getProfile.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Req, UseGuards } from "@nestjs/common";
+import { ShowProfile } from "@m/profile/service/showProfile.service";
+import { AuthTokenGuardAccess } from "@modules/auth/guard/access-token.guard";
+import { RequestUser } from "@modules/common/interfaces/interfaces";
+
+@Controller('profile')
+export class GetProfile{
+    constructor(private readonly showProfile: ShowProfile){}
+
+    @Get()
+    @UseGuards(AuthTokenGuardAccess)
+    async getProfile(@Req() req: RequestUser){
+        const userId = req.user.sub;
+        const role = req.user.role;
+
+        return this.showProfile.showProfileByRole(userId, role)
+    }
+}
