5c2676499e25d55ae55907b02eb24503856b1d33
RafaelSR0x <raffssantana007@gmail.com>
2026-05-04 16:55:17 -0300

    feat(profile-module): update module

--- src/modules/profile/module/profile.module.ts
+++ src/modules/profile/module/profile.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '@m/prisma/prisma.module';
 import { AuthModule } from '@m/auth/module/auth.module';
+import { ShowProfile } from '../service/showProfile.service';
+import { GetProfile } from '../controllers/getProfile.controller';
 @Module({
   imports: [AuthModule, PrismaModule],
-  providers: [],
-  controllers: [],
+  providers: [ShowProfile],
+  controllers: [GetProfile],
 })
 export class ProfileModule {}
