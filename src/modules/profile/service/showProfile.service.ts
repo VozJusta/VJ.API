f167b9256409df0b4e9ca5563aee719b03cb2812
RafaelSR0x <raffssantana007@gmail.com>
2026-05-04 20:36:01 -0300

    feat(profile-service): validation of i should return citizen's cpf or cnpj

--- src/modules/profile/service/showProfile.service.ts
+++ src/modules/profile/service/showProfile.service.ts
@@ -1,5 +1,8 @@
 import { PrismaService } from '@modules/prisma/service/prisma.service';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 
 @Injectable()
 export class ShowProfile {
@@ -24,12 +27,43 @@ export class ShowProfile {
           id: true,
           full_name: true,
           email: true,
+          cnpj: true,
           cpf: true,
+          avatar_image: true,
           phone: true,
         },
       });
 
-      return showInfoCitizen;
+      const cpfExists = showInfoCitizen?.cpf?.trim();
+      const cnpjExists = showInfoCitizen?.cnpj?.trim();
+
+      if (cpfExists) {
+        return {
+          id: showInfoCitizen?.id,
+          full_name: showInfoCitizen?.full_name,
+          email: showInfoCitizen?.email,
+          cpf: showInfoCitizen?.cpf,
+          avatar_image: showInfoCitizen?.avatar_image,
+          phone: showInfoCitizen?.phone,
+        };
+      } else if (cnpjExists) {
+        return {
+          id: showInfoCitizen?.id,
+          full_name: showInfoCitizen?.full_name,
+          email: showInfoCitizen?.email,
+          cnpj: showInfoCitizen?.cnpj,
+          avatar_image: showInfoCitizen?.avatar_image,
+          phone: showInfoCitizen?.phone,
+        };
+      } else {
+        return {
+          id: showInfoCitizen?.id,
+          full_name: showInfoCitizen?.full_name,
+          email: showInfoCitizen?.email,
+          avatar_image: showInfoCitizen?.avatar_image,
+          phone: showInfoCitizen?.phone,
+        };
+      }
     }
 
     if (userRole === 'lawyer') {
