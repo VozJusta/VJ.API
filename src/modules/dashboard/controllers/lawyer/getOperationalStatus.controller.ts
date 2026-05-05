6fac2d099dd77b56c5a79b46074c3a0afb3c43e7
RafaelSR0x <raffssantana007@gmail.com>
2026-05-05 16:54:53 -0300

    fix(dashboard): Adjustments were made to the return value of high-relevance to return values based on caseRequest, and adjustments were also made to the name and imports.

--- src/modules/dashboard/controllers/lawyer/getOperationalStatus.controller.ts
+++ src/modules/dashboard/controllers/lawyer/getOperationalStatus.controller.ts
@@ -1,6 +1,6 @@
 import { AuthTokenGuardAccess } from "@modules/auth/guard/access-token.guard";
 import { RequestUser } from "@m/common/interfaces/interfaces";
-import { OperationalStatusService } from "@modules/dashboard/service/lawyer/operetionalStatus.service";
+import { OperationalStatusService } from "@modules/dashboard/service/lawyer/operationalStatus.service";
 import { Get, UseGuards, Req, Controller } from "@nestjs/common";
 import { ApiBearerAuth, ApiOperation, ApiHeader, ApiResponse, ApiTags } from "@nestjs/swagger";
 
