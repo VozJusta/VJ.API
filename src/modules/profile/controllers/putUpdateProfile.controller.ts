f46ef472814f55d02a36ffb29fca64750403dd21
RafaelSR0x <raffssantana007@gmail.com>
2026-05-05 03:57:50 -0300

    feat(profile-controllers): controller for updating the lawyer and citizen data, treating the role in its own class

--- src/modules/profile/controllers/putUpdateProfile.controller.ts
+++ src/modules/profile/controllers/putUpdateProfile.controller.ts
@@ -1,6 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Put, Req, UseGuards } from '@nestjs/common';
+import { UpdateCitizenProfileService } from '@m/profile/service/updateCitizenProfile.service';
+import { UpdateLawyerProfileService } from '@m/profile/service/updateLawyerProfile.service';
+import { AuthTokenGuardAccess } from '@modules/auth/guard/access-token.guard';
+import { RequestUser } from '@modules/common/interfaces/interfaces';
 
 @Controller('profile')
 export class PutUpdateProfile {
-  constructor() {}
+  constructor(
+    private readonly updateCitizen: UpdateCitizenProfileService,
+    private readonly updateLawyer: UpdateLawyerProfileService,
+  ) {}
+
+  @Put()
+  @UseGuards(AuthTokenGuardAccess)
+  async putUpdateProfile(@Req() req: RequestUser, @Body() body: any) {
+    const userId = req.user.sub;
+    const role = req.user.role;
+
+    if (role === 'Citizen') {
+      return this.updateCitizen.updateCitizen(userId, role, body);
+    } else if (role === 'Lawyer') {
+      return this.updateLawyer.updateLawyer(userId, role, body);
+    }
+  }
 }
