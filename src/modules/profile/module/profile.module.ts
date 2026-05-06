8bc7e95f36585c4cf172e3f2273cb3b936ebe682
RafaelSR0x <raffssantana007@gmail.com>
2026-05-06 14:38:38 -0300

    feat(profile-module): update module

--- src/modules/profile/module/profile.module.ts
+++ src/modules/profile/module/profile.module.ts
@@ -3,12 +3,14 @@ import { PrismaModule } from '@m/prisma/prisma.module';
 import { AuthModule } from '@m/auth/module/auth.module';
 import { ShowProfileService } from '@m/profile/service/showProfile.service';
 import { GetProfileController } from '@m/profile/controllers/getProfile.controller';
-import { UpdateCitizenProfileService } from '../service/updateCitizenProfile.service';
-import { PutUpdateProfile } from '../controllers/putUpdateProfile.controller';
-import { UpdateLawyerProfileService } from '../service/updateLawyerProfile.service';
+import { UpdateCitizenProfileService } from '@m/profile/service/updateCitizenProfile.service';
+import { PutUpdateProfile } from '@m/profile/controllers/putUpdateProfile.controller';
+import { UpdateLawyerProfileService } from '@m/profile/service/updateLawyerProfile.service';
+import { UploadImageProfileService } from '@m/profile/service/uploadImageProfile.service';
+import { UploadProfileImageController } from '@m/profile/controllers/uploadProfileImage.controller';
 @Module({
   imports: [AuthModule, PrismaModule],
-  providers: [ShowProfileService, UpdateCitizenProfileService, UpdateLawyerProfileService],
-  controllers: [GetProfileController, PutUpdateProfile],
+  providers: [ShowProfileService, UpdateCitizenProfileService, UpdateLawyerProfileService, UploadImageProfileService],
+  controllers: [GetProfileController, PutUpdateProfile, UploadProfileImageController],
 })
 export class ProfileModule {}
