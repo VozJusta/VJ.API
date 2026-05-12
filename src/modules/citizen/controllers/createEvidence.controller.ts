3b160fecbb358c6f1d572bc28b733b3115844991
ooliveiratg <menezesth16@gmail.com>
2026-05-12 12:21:45 -0300

    feat(file): Adding validation related to image size.

--- src/modules/citizen/controllers/createEvidence.controller.ts
+++ src/modules/citizen/controllers/createEvidence.controller.ts
@@ -1,6 +1,7 @@
 import { AuthTokenGuardAccess } from '@modules/auth/guard/access-token.guard';
 import { RequestUser } from '@modules/common/interfaces/interfaces';
 import {
+  BadRequestException,
   Body,
   Controller,
   Post,
@@ -13,6 +14,27 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { CreateEvidenceService } from '../service/createEvidence.service';
 
 @Controller('me')
+@UseInterceptors(FileInterceptor('file', {
+  limits: {
+    fileSize:10 * 1024 * 1024
+  },
+   fileFilter: (req, file, callback) => {
+      const allowedMimeTypes = [
+        'image/jpeg',
+        'image/png',
+        'application/pdf',
+      ];
+
+      if (!allowedMimeTypes.includes(file.mimetype)) {
+        return callback(
+          new BadRequestException('Tipo de arquivo inválido'),
+          false,
+        );
+      }
+
+      callback(null, true);
+    },
+}))
 export class CreateEvidenceController {
   constructor(
     private readonly createEvidenceService: CreateEvidenceService
--- src/modules/citizen/service/createEvidence.service.ts
+++ src/modules/citizen/service/createEvidence.service.ts
@@ -1,45 +1,60 @@
-import { PrismaService } from "@modules/prisma/service/prisma.service";
-import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
-import { CloudinaryService } from "./cloudinary.service";
+import { PrismaService } from '@modules/prisma/service/prisma.service';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { CloudinaryService } from './cloudinary.service';
 import Tesseract from 'tesseract.js';
 
 @Injectable()
 export class CreateEvidenceService {
-    constructor(
-        private readonly prisma: PrismaService,
-        private readonly cloudinaryService: CloudinaryService,
-    ) { }
-    async createEvidence(file: Express.Multer.File, userId: string, role: string) {
-        const userRole = role.toLowerCase();
-        if (userRole !== 'citizen') {
-            throw new UnauthorizedException('Apenas cidadãos podem criar evidências');
-        }
-
-        if (!file) {
-            throw new BadRequestException('Arquivo é obrigatório');
-        }
-
-        const citizenId = await this.prisma.citizen.findUnique({
-            where: { id: userId },
-        });
-        if (!citizenId) {
-            throw new NotFoundException('Cidadão não encontrado');
-        }
-
-        const upload = await this.cloudinaryService.uploadFile(file);
-        console.log(upload);
-
-        const ocr = await Tesseract.recognize(file.buffer, 'eng');
-
-        const evidence = await this.prisma.evidence.create({
-            data: {
-                file_url: upload.secure_url,
-                ocr_content: ocr.data.text,
-                public_id: upload.public_id,
-
-            }
-        })
-        return evidence
-
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly cloudinaryService: CloudinaryService,
+  ) {}
+  async createEvidence(
+    file: Express.Multer.File,
+    userId: string,
+    role: string,
+  ) {
+    const userRole = role.toLowerCase();
+    if (userRole !== 'citizen') {
+      throw new UnauthorizedException('Apenas cidadãos podem criar evidências');
     }
+
+    if (!file) {
+      throw new BadRequestException('Arquivo é obrigatório');
+    }
+
+    const maxSizeFile = 10 * 1024 * 1024;
+
+    if (file.size > maxSizeFile) {
+      throw new BadRequestException(
+        'Arquivo excede o tamanho máximo permitido de 10MB',
+      );
+    }
+
+    const citizenId = await this.prisma.citizen.findUnique({
+      where: { id: userId },
+    });
+    if (!citizenId) {
+      throw new NotFoundException('Cidadão não encontrado');
+    }
+
+    const upload = await this.cloudinaryService.uploadFile(file);
+    console.log(upload);
+
+    const ocr = await Tesseract.recognize(file.buffer, 'eng');
+
+    const evidence = await this.prisma.evidence.create({
+      data: {
+        file_url: upload.secure_url,
+        ocr_content: ocr.data.text,
+        public_id: upload.public_id,
+      },
+    });
+    return evidence;
+  }
 }
