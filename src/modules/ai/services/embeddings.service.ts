ce966d6037dcd378c2f1092a23b8617bec289df2
cau4alves <cau4alves@gmail.com>
2026-06-22 21:59:36 -0300

    feat(ai): adjust embedding

--- src/modules/ai/services/embeddings.service.ts
+++ src/modules/ai/services/embeddings.service.ts
@@ -9,14 +9,14 @@ export class EmbeddingsService {
     async generate(text: string): Promise<number[]> {
         const response = await firstValueFrom(
             this.httpService.post(
-                'https://api.openai.com/v1/embeddings',
+                'https://openrouter.ai/api/v1/embeddings',
                 {
-                    model: 'text-embedding-3-small',
+                    model: 'nvidia/llama-nemotron-embed-vl-1b-v2:free',
                     input: text,
                 },
                 {
                     headers: {
-                        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+                        Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`
                     }
                 }
             )
